68cf591e9ade9c5557ee2bf3315a6952c8e51743
Joshua Lester <jlester3@ewu.edu>
2024-04-09 08:49:57 -0700

    added toolbar with options

---
+++ practice-web/layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- <v-app>
+    <v-toolbar app height="150" class="indigo">
+      <v-btn to="/">Home</v-btn>
+      <v-btn to="/about">About</v-btn>
+    </v-toolbar>
+    <v-main>
+      <slot />
+    </v-main>
+  </v-app> -->
+  <v-layout>
+    <v-toolbar height="25">
+      <template v-slot:prepend>
+        <v-btn density="compact">File</v-btn>
+        <v-btn density="compact">Edit</v-btn>
+        <v-btn density="compact">Etc.</v-btn>
+      </template>
+    </v-toolbar>
+  </v-layout>
+</template>
