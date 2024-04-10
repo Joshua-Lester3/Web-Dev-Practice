7b97d5d6669aa78fdf86d974d91ef955e67f7393
Joshua Lester <jlester3@ewu.edu>
2024-04-10 14:48:52 -0700

    Vue with typescript

---
+++ vue-project/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <Component header="wassup" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Component from './components/Component.vue';
+
+export default defineComponent({
+  components: { Component },
+});
+</script>
---
+++ vue-project/src/components/Component.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{ header }}</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: ['header'],
+});
+</script>
+
+<style scoped>
+h1 {
+  color: black;
+}
+</style>
