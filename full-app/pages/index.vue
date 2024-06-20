79750798949abd55e5c4921b1089737b758a056d
Joshua Lester <jlester3@ewu.edu>
2024-06-20 14:38:57 -0700

    Saving changes

--- full-app/pages/index.vue
+++ full-app/pages/index.vue
@@ -1,3 +1,49 @@
 <template>
-  <h1>Hello!</h1>
+  <v-btn class="ma-5" @click="count++">Plus me!</v-btn>
+  <v-card
+    width="50"
+    height="50"
+    class="ma-5 align-center d-flex justify-center elevation-7"
+    style="background-image: radial-gradient(red 5%, blue 75%)"
+    >{{ count }}</v-card
+  >
+
+  <v-infinite-scroll :height="300" :items="items" :onLoad="load">
+    <template v-for="(item, index) in items" :key="item">
+      <div :class="['pa-2', index % 2 === 0 ? 'bg-grey-lighten-2' : '']">
+        Item number #{{ item }}
+      </div>
+    </template>
+  </v-infinite-scroll>
 </template>
+
+<script setup lang="ts">
+import { useTheme } from 'vuetify';
+
+const theme = useTheme();
+theme.global.name;
+const count = ref(0);
+
+const items = ref(Array.from({ length: 30 }, (k, v) => v + 1));
+
+async function api(): Promise<number[]> {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(getNumbers());
+    }, 1000);
+  });
+}
+function getNumbers() {
+  let array = new Array<number>();
+  return array;
+}
+async function load({ done }: { done: any }) {
+  // Perform API call
+  const res = await api();
+  for (let i in res) {
+    items.value.push(parseInt(i));
+  }
+
+  done('ok');
+}
+</script>
--- full-app/plugins/vuetify.ts
+++ full-app/plugins/vuetify.ts
@@ -7,6 +7,26 @@ import { createVuetify } from 'vuetify';
 export default defineNuxtPlugin(app => {
   const vuetify = createVuetify({
     // ... your configuration
+    theme: {
+      defaultTheme: 'light',
+      themes: {
+        light: {
+          dark: false,
+          colors: {
+            primary: '#FFD54F',
+            secondary: '#9575CD',
+            error: '#FF5252',
+            info: '#2196F3',
+            success: '#4CAF50',
+            warning: '#FFC107',
+            correct: '#4CAF50',
+            misplaced: '#FFC107',
+            wrong: '#BDBDBD',
+            unknown: '#d6d6d6',
+          },
+        },
+      },
+    },
   });
   app.vueApp.use(vuetify);
 });
